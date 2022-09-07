977d078497059c1d037d2882294e0fe5541def3e
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-09-07 09:34:42 +0430

    Remove current endpoint app.use

--- AspNetCoreEndpoints/Program.cs
+++ AspNetCoreEndpoints/Program.cs
@@ -26,31 +26,6 @@ app.Use(async (context, next) =>
 
 app.UseRouting();
 
-app.Use(async (context, next) =>
-{
-    var currentEndpoint = context.GetEndpoint();
-
-    if (currentEndpoint is null)
-    {
-        await next(context);
-        return;
-    }
-
-    Console.WriteLine($"Endpoint: {currentEndpoint.DisplayName}");
-
-    if (currentEndpoint is RouteEndpoint routeEndpoint)
-    {
-        Console.WriteLine($"  - Route Pattern: {routeEndpoint.RoutePattern}");
-    }
-
-    foreach (var endpointMetadata in currentEndpoint.Metadata)
-    {
-        Console.WriteLine($"  - Metadata: {endpointMetadata}");
-    }
-
-    await next(context);
-});
-
 app.UseAuthorization();
 
 app.UseCustomEndpoint("/my-endpoint", message =>
